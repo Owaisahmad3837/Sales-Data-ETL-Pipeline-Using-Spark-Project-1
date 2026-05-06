5f87413fad27fb91f98b9f9cca93ea40f14406ca
Owais Ahmad <owaisahmad3837@gmail.com>
2026-05-07 04:46:56 +0500

    Initial commit

--- etl/load.py
+++ etl/load.py
@@ -1,4 +1,6 @@
+import os
 def load(df):
+    os.makedirs("Output",exist_ok=True)
     df.write \
         .format("jdbc") \
         .option("url", "jdbc:postgresql://localhost:5432/spark") \
@@ -6,5 +8,7 @@ def load(df):
         .option("user", "owais") \
         .option("password", "owais7383") \
         .option("driver", "org.postgresql.Driver") \
-        .mode("write") \
+        .mode("append") \
         .save()
+    
+    df.write.mode("overwrite").option("header",True).csv("Output/clean_sales.csv")
